10caa0177a084d41cdb9a68a1ce5953795412d20
MANSI BAJAJ <bmansi362@gmail.com>
2023-01-13 21:46:24 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Product p = new Product();
+        System.out.print(p.product(4, 5) + " " + p.product(3, 5, 6) + " " + p.product(2.3, 4.6));
+    }
+
+    public static class Product {
+        public int product(int x, int y) {
+            return x + y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x + y + z;
+        }
+
+        public double product(double x, double y) {
+            return x + y;
+        }
+    }
 }
